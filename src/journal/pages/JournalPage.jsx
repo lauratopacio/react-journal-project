2270813e64fe7446bf327ce2c3b75acc22217fe6
Laura Valdez <lauratopaciovaldez@gmail.com>
2022-10-21 12:02:05 -0600

    subir y elminiar notas e imagenes

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,46 +1,46 @@
-import { IconButton } from "@mui/material"
-import { AddOutlined } from "@mui/icons-material"
+import { useDispatch, useSelector } from 'react-redux';
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
 
-import { JournalLayout } from "../layout/JournalLayout"
-import { NoteView, NothingSelectedView } from "../views"
-import { useDispatch, useSelector } from "react-redux"
-import { startNewNote } from "../../store/journal"
+import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
+import { startNewNote } from '../../store/journal/thunks';
 
 export const JournalPage = () => {
-  const dispatch = useDispatch();
 
-  const {isSaving, active} =useSelector(state=> state.journal);
+  const dispatch = useDispatch();
+  const { isSaving, active } = useSelector( state => state.journal );
 
   const onClickNewNote = () => {
-    dispatch(startNewNote());
-
+    dispatch( startNewNote() );
   }
 
   return (
     <JournalLayout>
+      
       {
         (!!active)
-        ? <NoteView />
-        : <NothingSelectedView />
+          ? <NoteView />
+          : <NothingSelectedView />
       }
-       
 
-      <IconButton 
-        onClick={onClickNewNote}
+
+      <IconButton
+        onClick={ onClickNewNote }
         size='large'
-        disabled={isSaving}
+        disabled={ isSaving }
         sx={{
-          color:'white',
-          backgroundColor:'error.main',
-          ':hover':{ backgroundColor:'error.main', opacity:'0.9'},
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
           position: 'fixed',
-          right:50,
-          bottom:50
+          right: 50,
+          bottom: 50
         }}
       >
-        <AddOutlined sx={{fontSize:30}}/>
+        <AddOutlined sx={{ fontSize: 30 }} />
       </IconButton>
+
     </JournalLayout>
-    
   )
 }
--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -24,18 +24,41 @@ export const journalSlice = createSlice({
         },
         setActiveNote: (state, action) => {
             state.active= action.payload;
+            state.messageSaved='';
         },
         setNotes: (state, action) => {
             state.notes=action.payload;
         },
         setSaving: (state) => {
-
+            state.isSaving=true;
+            //TODO: mensaje de error...
+            state.messageSaved='';
         },
         updateNote: (state, action) => {
-
+            state.isSaving=false;
+            state.notes=state.notes.map(note=> {
+                if(note.id=== action.payload.id){
+                    return action.payload;
+                }
+                return note;
+            });
+            //Todo: mostrar mensaje de actualizacion
+            state.messageSaved=`${action.payload.title}, actualizada corectamente`;
+        },
+        setPhotosToActiveNote: (state,action) => {
+            state.active.imageUrls = [...state.active.imageUrls, ...action.payload];
+            state.isSaving=false;
+        },
+        clearNotesLogout:(state) => {
+            state.isSaving=false;
+            state.messageSaved = '';
+            state.notes=[];
+            state.active = null;
         },
         deleteNoteById: (state, action) => {
-
+            state.active=null;
+            state.notes=state.notes.filter(note => note.id !== action.payload);
+            
         }
 
     }
@@ -43,4 +66,4 @@ export const journalSlice = createSlice({
 
 
 // Action creators are generated for each case reducer function
-export const { savingNewNote, addNewEmptyNote, setActiveNote, setNotes, setSaving, updateNote, deleteNoteById} = journalSlice.actions;
+export const { savingNewNote, addNewEmptyNote, setActiveNote, setNotes, setSaving,clearNotesLogout, updateNote, setPhotosToActiveNote, deleteNoteById} = journalSlice.actions;
